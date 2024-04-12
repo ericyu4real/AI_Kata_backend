71dde75d3d27428b797e0b9ee67834ed0c3617de
Your Name <you@example.com>
2024-04-12 17:24:23 -0400

    Cherry on top

--- main.py
+++ main.py
@@ -32,16 +32,15 @@ db = FAISS.load_local("faiss_index",
 
 # Build prompt
 template = """You are a helpful AI assistant working for MScAC (The Master of Science in Applied Computing) at the University of Toronto, the best CS master program in Canada. This program offers a unique combination of academic research and industry engagement. The program aims to cultivate world-class innovators through rigorous education in state-of-the-art research techniques, culminating in an applied research internship. It offers concentrations in fields like Applied Mathematics, Artificial Intelligence, Computer Science, Data Science, and more. And your name is Claire. There are a few links you can give it to user if they asked questions related with them: 
-1. MScAC Official Website: https://mscac.utoronto.ca/
-2. MScAC Application Portal: https://admissions.sgs.utoronto.ca/apply/
-3. Student Care Website: https://www.studentcare.ca/
-4. CS Course Timetable: https://web.cs.toronto.edu/graduate/timetable
-5. Statistics Course Timetable: https://www.statistics.utoronto.ca/graduate-timetable/current-upcoming-timetable
-6. Acorn Login: https://www.acorn.utoronto.ca/
-7. Quecus Website: https://q.utoronto.ca/
-8. Leetcode Practice: https://leetcode.com/problemset/
-9. Career & Co-Corricular Learning Network: https://clnx.utoronto.ca/notLoggedIn.htm
-10. Mental Health Service at UofT: https://studentlife.utoronto.ca/service/mental-health-clinical-services/
+MScAC Application Portal: https://admissions.sgs.utoronto.ca/apply/
+Insurance Website: https://www.studentcare.ca/
+CS Course Timetable: https://web.cs.toronto.edu/graduate/timetable
+Statistics Course Timetable: https://www.statistics.utoronto.ca/graduate-timetable/current-upcoming-timetable
+Acorn Login: https://www.acorn.utoronto.ca/
+Quecus Website: https://q.utoronto.ca/
+Leetcode Practice: https://leetcode.com/problemset/
+Career & Co-Corricular Learning Network: https://clnx.utoronto.ca/notLoggedIn.htm
+Mental Health Service at UofT: https://studentlife.utoronto.ca/service/mental-health-clinical-services/
 Also, use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
 {context}
 Question: {question}
@@ -67,10 +66,17 @@ def index():
 def query():
     try:
         query_text = request.form.get('query')
+        chat_history_json = request.form.get('history')
         if not query_text:
             return jsonify({'error': "Please enter a query."}), 400
+        
+        if chat_history_json:
+            chat_history_raw = json.loads(chat_history_json)
+            chat_history = [(entry['user_message']['body'], entry['bot_message']['body']) for entry in chat_history_raw]
+        else:
+            chat_history = []
 
-        result = qa({"question": query_text, "chat_history": []})
+        result = qa({"question": query_text, "chat_history": chat_history})
         if result['answer']:
             success = save_message(f"User: {query_text}; Bot: {result['answer']}")
             if success:
